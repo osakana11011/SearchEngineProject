8c8f17cfc250b1285b5c0c40cd7775c84c068f9d
osakana11011 <taka.knk.11011@gmail.com>
2020-01-06 02:51:21 +0900

    単語追加処理の無駄なメソッドを削除

--- crawler/domain/repository/word_repository.go
+++ crawler/domain/repository/word_repository.go
@@ -2,7 +2,6 @@ package repository
 
 // WordRepository ...
 type WordRepository interface {
-	Regist(word string) (int64, error)
 	BulkInsert(words []string) error
 	GetID(word string) (int64, error)
 	GetCounts(word string) (int, error)
