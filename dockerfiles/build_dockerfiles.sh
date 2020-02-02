d1415ead727fe935a6a40f780ed435ea6a678562
osakana11011 <taka.knk.11011@gmail.com>
2020-02-02 19:36:30 +0900

    search_engineコンテナにDIコンテナライブラリを導入

--- dockerfiles/build_dockerfiles.sh
+++ dockerfiles/build_dockerfiles.sh
@@ -1,7 +1,7 @@
 # Crawler
-docker build -t kaneko11011/crawler:0.0.3 ./crawler
-docker push kaneko11011/crawler:0.0.3
+docker build -t kaneko11011/crawler:0.0.4 ./crawler
+docker push kaneko11011/crawler:0.0.4
 
 # SearchEngine
-docker build -t kaneko11011/search_engine:0.0.3 ./search_engine
-docker push kaneko11011/search_engine:0.0.3
+docker build -t kaneko11011/search_engine:0.0.4 ./search_engine
+docker push kaneko11011/search_engine:0.0.4
