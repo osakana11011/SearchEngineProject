93e00ce0b309ae093fcb7b3c7e99dc2c80ff11fd
osakana11011 <taka.knk.11011@gmail.com>
2020-02-02 18:37:41 +0900

    gorm-bulk-insertを導入

--- dockerfiles/build_dockerfiles.sh
+++ dockerfiles/build_dockerfiles.sh
@@ -1,2 +1,7 @@
-docker build -t kaneko11011/crawler:0.0.2 ./crawler
-docker push kaneko11011/crawler:0.0.2
+# Crawler
+docker build -t kaneko11011/crawler:0.0.3 ./crawler
+docker push kaneko11011/crawler:0.0.3
+
+# SearchEngine
+docker build -t kaneko11011/search_engine:0.0.3 ./search_engine
+docker push kaneko11011/search_engine:0.0.3
